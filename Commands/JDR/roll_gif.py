509e5928bad492f626d4a37227d8a057d608e4ca
iElden <mceldenn@gmail.com>
2020-01-08 18:30:52 +0100

    JDR: Added gif

--- Commands/JDR/roll_gif.py
+++ Commands/JDR/roll_gif.py
@@ -1,5 +1,7 @@
 SUCCES_GIF = [
     "https://media.tenor.com/images/f39f775227250c05070baa7d6151e8dc/tenor.gif"
+    "https://media.tenor.com/images/baa1b555e5c312ea6917c4187a91a44f/tenor.gif"
+
 ]
 
 FAIL_GIF = [
