4113732017a90953ff294a12c77fe223202b1923
iElden <mceldenn@gmail.com>
2020-07-07 00:21:09 +0200

    Added max civ per draft in CIVFR /draft

--- Commands/CivFR/CivFR.py
+++ Commands/CivFR/CivFR.py
@@ -11,6 +11,7 @@ class CmdCivFR:
         if not args[0].isdigit():
             raise InvalidArgs("1st Argument must be a integer (exemple: ``/draft 2``)")
         pool = leaders.leaders[:]
+        nb = int(args[0])
         if len(args) >= 2:
             ban_query = args[1].split('.')
             for ban in ban_query:
@@ -18,9 +19,14 @@ class CmdCivFR:
                 if not lead:
                     raise InvalidArgs(f"Leader \"{ban}\" non trouvé")
                 pool.remove(lead)
-        nb = int(args[0])
+        if len(args) >= 3:
+            if not args[2].isdigit():
+                raise InvalidArgs("3rd Argument (max civ per draft) must be a integer (exemple: ``/draft 8 Maori.Colombie 4``)")
+            leader_per_player = int(args[2])
+        else:
+            leader_per_player = len(pool) // nb
         random.shuffle(pool)
-        leader_per_player = len(pool) // nb
+
         result = []
         for i in range(nb):
             g = (f"{client.get_emoji(j.emoji_id)} {j.uuname.title()}" for j in pool[i*leader_per_player:i*leader_per_player+leader_per_player])
