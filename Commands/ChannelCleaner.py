915a35207ec7f667d96d84bda941bbafab8abce2
iElden <mceldenn@gmail.com>
2020-09-10 17:50:07 +0200

    Modified Channel Clearer

--- Commands/ChannelCleaner.py
+++ Commands/ChannelCleaner.py
@@ -1,19 +1,23 @@
 import discord
 import logging
 from datetime import datetime, timedelta
+from typing import Dict
 
 logger = logging.getLogger("ChannelCleaner")
 
-CHANNELS = [516418255834054671]
+CHANNELS : Dict[int, timedelta] = {
+    516418255834054671: timedelta(hours=24),
+    571736666868285455: timedelta(days=30)
+}
 
 def message_is_not_pinned(message: discord.Message):
     return not message.pinned
 
-async def clear_channels(channel : discord.TextChannel):
-    await channel.purge(limit=None, before=datetime.now() - timedelta(hours=24), check=message_is_not_pinned)
+async def clear_channels(channel : discord.TextChannel, delta : timedelta):
+    await channel.purge(limit=None, before=datetime.now() - delta, check=message_is_not_pinned)
 
 
 async def routine(client : discord.Client):
     logger.debug("Runing clear routine ...")
-    for channel_id in CHANNELS:
-        await clear_channels(client.get_channel(channel_id))
+    for channel_id, delta in CHANNELS.items():
+        await clear_channels(client.get_channel(channel_id), delta)
